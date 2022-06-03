672541f970aae30b1f34c7a94eee9c75dbc5442d
Sam231221 <rasengankid999@gmail.com>
2022-06-03 23:16:05 +0545

    production build added

---
+++ build/precache-manifest.c2e3b56842aa54432ddcce6271fed8ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de81f0caa5c1b65f609c58393adac1f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f891effa47c9c0f7a534",
+    "url": "/static/js/2.720518cb.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/static/js/2.720518cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84332d21863f03b1b2b7",
+    "url": "/static/js/main.f0fde8f3.chunk.js"
+  },
+  {
+    "revision": "5dcc4a2d51d1022f96e5",
+    "url": "/static/js/runtime-main.edb82d6c.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/static/media/svg-3.397c455a.svg"
+  }
+]);
